e626ecd9d72896dd5c85011826df85510edc8bc9
Ayman Munassar <43368089+AymanMunassar@users.noreply.github.com>
2024-05-16 08:22:39 +0300

    Update ServiceSniffer.py

--- ServiceSniffer.py
+++ ServiceSniffer.py
@@ -11,57 +11,84 @@ Prerequists: install scapy by running pip install scapy
 # Written by Ayman Munassar
 
 
-import xml.etree.ElementTree as ET
+import json
 import time
-from scapy.all import *
+import socket
+import struct
 import threading
 
-dest_ip = "127.0.0.1" #Change me - IP to monitor
-tcp_ports = [443, 80, 22, 53] #Change me - Ports to monitor
+target_ip = "127.0.0.1" #Change me - IP to monitor
+tcp_ports = [10000, 10001, 10002, 10003, 10004, 10005] #Change me - Ports to monitor
 timer = 59 # Change me - Refresh in seconds
+interface = 'eth0' # Change me - Interface to monitor
 data = []
 results = {}
 
+def count_sms(port):
+    conn = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(3))#socket.htons(3) = ipv4 
+    conn.bind((interface, 0))
+    packet_count = 0
+    timeout = time.time() + timer
+    while time.time() < timeout:
+        raw_data, _ = conn.recvfrom(65535)
+        # Parsing Ethernet
+        dest_mac, src_mac, eth_proto = struct.unpack('!6s6sH', raw_data[:14])
+        #src_mac_str = ':'.join(format(x, '02x') for x in src_mac)
+        #dest_mac_str = ':'.join(format(x, '02x') for x in dest_mac)
+        if dest_mac[0] & 1 == 0:
+            #Parsing IP
+            ip_header = raw_data[14:34]
+            #ip_header_length = (ip_header[0] & 15) * 4
+            src_ip = socket.inet_ntoa(ip_header[12:16])
+            #dest_ip = socket.inet_ntoa(ip_header[16:20])
+            if src_ip == target_ip: #or dest_ip == target_ip:
+                #Parsing TCP
+                tcp_header = struct.unpack('!HHLLBBHHH', raw_data[34:54])
+                source_port, dest_port = tcp_header[0], tcp_header[1]
+                if dest_port == port: #or dest_port == port:
+                    packet_count += 1
+    return packet_count
 
-def count_packets(port):
-    #You can mnipulate the filter as you need with Berkeley Packet Filter (BPF)
-    packets = sniff(filter=f"ip host {dest_ip} and tcp port {port}", timeout=timer)
-    return len(packets)
-
-def save_to_xml():
-    file_path =  "Packet_Counter.xml"
-    root = ET.Element("Packet_Counts")
+def save_to_json(data):
+    file_path = "SMS_Counter.json"
+    result =  {"SMS_Counts": []}
+    #print(data)
     for entry in data:
-        packet = ET.SubElement(root, "Counter_Info")
-        packet.set("Timestamp", entry["timestamp"])
         allTuples = list(entry.items())
         secondTuple = allTuples[1:2]
-        
         for port, count in secondTuple:
-            port_elem = ET.SubElement(packet, "Port")
-            port_elem.set("Number", str(port).rsplit('_', 1)[1])
-            port_elem.set("Packet_count", str(count))
+            timestamp = int(entry["timestamp"])
+            count = str(count)
+            port = str(port).rsplit('_', 1)[1]
+            packet = {
+                "Port": port,
+                "Counter_Info": {
+                    "Timestamp": timestamp,
+                    "Count": count
+                }
+            }
+        result["SMS_Counts"].append(packet)
     
-    tree = ET.ElementTree(root)
-    tree.write(file_path)
+    with open(file_path, 'w') as file:
+        json.dump(result, file, indent=4)
 
 def threadedCounting():
     data.clear()
     threads = []
     for port in tcp_ports:
-        thread = threading.Thread(target=lambda res, f, val: res.update({f.__name__ + "_" + str(val): f(val)}), args=(results, count_packets , port))
+        thread = threading.Thread(target=lambda res, f, val: res.update({f.__name__ + "_" + str(val): f(val)}), args=(results, count_sms , port))
         threads.append(thread)
         thread.start()
     for thread in threads:
         thread.join()
     for key, value in results.items():
-        data.append({"timestamp":time.ctime(),key:value})
+        data.append({"timestamp":int(time.time()),key:value})
 
 def main():
     while True:
         threadedCounting()
-        save_to_xml()
+        save_to_json(data)
         time.sleep(1)
-
+        
 if __name__ == "__main__":
     main()
